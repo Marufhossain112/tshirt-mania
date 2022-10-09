3a7f4f615722527bd9ed5e0288aa4cc10b501e14
maruf <dev.maruf112@gmail.com>
2022-10-09 07:31:20 +0600

    cart cond render btn added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 const Cart = ({ cart, handleRemoveCart }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please buy at least one item.</p>;
+  } else if (cart.length === 1) {
+    message = (
+      <div>
+        <p>Moore jonnon kino</p>
+        <p>Moore baher lagi kino</p>
+        <p>Moore dardar lagi kino</p>
+      </div>
+    );
+  } else {
+    message = <p>Thanks for purchasing !</p>;
+  }
   return (
     <div>
       <h2>This is a Cart.</h2>
@@ -11,6 +25,7 @@ const Cart = ({ cart, handleRemoveCart }) => {
           <button onClick={() => handleRemoveCart(tshirt)}>X</button>
         </p>
       ))}
+      {message}
     </div>
   );
 };
